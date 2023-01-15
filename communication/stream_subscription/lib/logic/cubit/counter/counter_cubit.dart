ceddb9cecb2c1077198e8591aff50c65e9b554eb
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-01-15 10:42:10 +0530

    develop counter cubit

--- communication/stream_subscription/lib/logic/cubit/counter/counter_cubit.dart
+++ communication/stream_subscription/lib/logic/cubit/counter/counter_cubit.dart
@@ -1,8 +1,14 @@
+import 'dart:html';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterInitial());
+  CounterCubit() : super(const CounterInitial(counter: 0));
+
+  void increment() => emit(CounterIncrement(counter: state.counter + 1));
+
+  void decrement() => emit(CounterDecrement(counter: state.counter - 1));
 }
--- communication/stream_subscription/lib/logic/cubit/counter/counter_state.dart
+++ communication/stream_subscription/lib/logic/cubit/counter/counter_state.dart
@@ -1,6 +1,25 @@
 part of 'counter_cubit.dart';
 
 @immutable
-abstract class CounterState {}
+abstract class CounterState {
+  final int counter = 0;
+}
 
-class CounterInitial extends CounterState {}
+class CounterInitial implements CounterState {
+  @override
+  final int counter;
+
+  const CounterInitial({required this.counter});
+}
+
+class CounterIncrement implements CounterState {
+  @override
+  final int counter;
+  const CounterIncrement({required this.counter});
+}
+
+class CounterDecrement implements CounterState {
+  @override
+  final int counter;
+  const CounterDecrement({required this.counter});
+}
