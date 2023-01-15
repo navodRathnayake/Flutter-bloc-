4a20f1bf9143ad06b31d8c48ec0a2cd119388178
Navod Rathnayake <82445185+navodRathnayake@users.noreply.github.com>
2023-01-15 11:09:08 +0530

    internet cubit and stream subscription has developed

--- communication/stream_subscription/lib/logic/cubit/network/network_cubit.dart
+++ communication/stream_subscription/lib/logic/cubit/network/network_cubit.dart
@@ -1,8 +1,36 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:meta/meta.dart';
 
 part 'network_state.dart';
 
 class NetworkCubit extends Cubit<NetworkState> {
-  NetworkCubit() : super(NetworkInitial());
+  final Connectivity connectivity;
+  StreamSubscription connectivitySubscription;
+  NetworkCubit(
+      {required this.connectivity, required this.connectivitySubscription})
+      : super(NetworkLoading()) {
+    listenSubscription();
+  }
+
+  void emitInternetConnected({required NetworkStatus status}) =>
+      emit(NetworkConnected(status: status)); //
+
+  void emitInternetDisconnected() =>
+      emit(NetworkDisconnected(status: NetworkStatus.none));
+
+  void listenSubscription() {
+    connectivitySubscription =
+        connectivity.onConnectivityChanged.listen((ConnectivityResult result) {
+      if (result == ConnectivityResult.wifi) {
+        emitInternetConnected(status: NetworkStatus.wifi);
+      } else if (result == ConnectivityResult.mobile) {
+        emitInternetConnected(status: NetworkStatus.mobile);
+      } else if (result == ConnectivityResult.none) {
+        emitInternetDisconnected();
+      }
+    });
+  }
 }
--- communication/stream_subscription/lib/logic/cubit/network/network_state.dart
+++ communication/stream_subscription/lib/logic/cubit/network/network_state.dart
@@ -1,6 +1,18 @@
 part of 'network_cubit.dart';
 
+enum NetworkStatus { mobile, wifi, none }
+
 @immutable
 abstract class NetworkState {}
 
-class NetworkInitial extends NetworkState {}
+class NetworkLoading extends NetworkState {}
+
+class NetworkConnected extends NetworkState {
+  final NetworkStatus status;
+  NetworkConnected({required this.status});
+}
+
+class NetworkDisconnected extends NetworkState {
+  final NetworkStatus status;
+  NetworkDisconnected({required this.status});
+}
